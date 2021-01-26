7d0f2abf6402e8878d216fa557b19e3bf541afd5
xD0m1n1c <dominic_bachmann@sluz.ch>
2021-01-26 19:35:04 +0100

    Added nodejs

---
+++ node/wasm/cryptli.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} str
+* @param {number} shift_amount
+* @returns {string}
+*/
+export function cesar(str: string, shift_amount: number): string;
